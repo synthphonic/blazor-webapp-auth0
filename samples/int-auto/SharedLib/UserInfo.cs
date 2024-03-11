029795198514d1f42ab3c06729670dbf68a0a4ed
Shah Z. S <shahzshafie@outlook.com>
2024-03-11 17:05:35 +0800

    add sharedlib

---
+++ samples/int-auto/SharedLib/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedLib;
+
+public class UserInfo
+{
+  public string? UserId { get; set; }
+  public string? Name { get; set; }
+  public string? Email { get; set; }
+  public string? Picture { get; set; }
+  public string? Sid { get; set; }
+  public string? NameIdentifier { get; set; }
+  public Dictionary<string, string> ClaimsDictionary { get; set; } = [];
+}
